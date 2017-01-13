aaff2579fed6ff9f5c915ef5282b4b1721ac7a1a
Fabiola Ampuero <fabi.ampuero.chaclco@gmail.com>
2017-01-13 16:38:14 -0500

    se agrego archivos

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function validateForm(){
+	/* Escribe tú código aquí */
+    var formulario=document.getElementsByClassName("form-signup")
+    var nombre, apellido, correo, contraseña, tipo, twitter, check, boton;
+    nombre=document.getElementById("name").value;
+    apellido=document.getElementById("lastname").value;
+    correo=document.getElementById("input-email").value;
+    contraseña=document.getElementById("input-password").value;
+    
+    var nombreArray=nombre.split();
+    for(var i=0;i<nombreArray.length;i++){
+        if(nombreArray[0]!=)
+    }
+    if()
+}
